ecf5ffff53e71786d64b47a644375e1d2e301c26
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-05-06 20:06:25 +0200

    chore: Update database connection configuration

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,9 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 
 import { Pool } from 'pg';
-const pool = new Pool();
 
-const db = drizzle(pool);
+const pool = new Pool({
+  connectionString: Bun.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, {});

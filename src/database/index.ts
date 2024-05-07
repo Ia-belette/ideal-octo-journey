5e751f7d7a4c49a975dd20b91d3c9f8c5e977dfc
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-05-07 13:21:59 +0200

    chore: Update database connection configuration and add content schemas

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,16 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-
 import { Pool } from 'pg';
 
+import { categories, contents, contentFlag } from './schemas/content';
+
 const pool = new Pool({
-  connectionString: Bun.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
-export const db = drizzle(pool, {});
+export const db = drizzle(pool, {
+  schema: {
+    contents,
+    categories,
+    content_flag: contentFlag,
+  },
+});

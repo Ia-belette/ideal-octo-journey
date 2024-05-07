b6d90625befd1e8df367a5f8e63f05d37bb9d2a8
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-05-07 14:33:38 +0200

    chore: update endpoint for retrieving contents and database connection configuration

--- src/controllers/contents/index.ts
+++ src/controllers/contents/index.ts
@@ -1,7 +1,18 @@
 import { Hono } from 'hono';
-import { db } from '../../database/index';
-const app = new Hono();
+import { Env } from '../../types';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { contents } from '../../database/schemas/content';
+
+export const app = new Hono<{ Bindings: Env }>();
 
 app.get('/contents', async (c) => {
-  c.json({ contents: [] });
+  try {
+    const sql = neon(c.env.DATABASE_URL);
+    const db = drizzle(sql);
+    const results = await db.select().from(contents);
+    return c.json(results);
+  } catch (error) {
+    return c.json({ error }, 400);
+  }
 });

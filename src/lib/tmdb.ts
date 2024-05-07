a39e89eaf42a5e815ec669338e7a91bc72b62512
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-05-07 22:47:15 +0200

    feat(tmdb): Add TMDB findByIdImdb function to fetch TV show details

---
+++ src/lib/tmdb.ts
@@ -0,0 +1,24 @@
+type TvResults = {
+  backdrop_path: string;
+  poster_path: string;
+  id: number;
+};
+
+export const findByIdImdb = async (
+  id: string,
+  tmdbApiKey: string,
+  language: 'fr' | 'en' = 'fr'
+) => {
+  const url = `https://api.themoviedb.org/3/find/${id}?external_source=imdb_id&language=${language}`;
+  
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${tmdbApiKey}`,
+    },
+  };
+
+  const response = await fetch(url, options);
+  return response.json() as Promise<{ tv_results: TvResults[] }>;
+};

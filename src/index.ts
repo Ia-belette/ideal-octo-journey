2a0089dd796b9c245f3445fc26cb703dd3129ab1
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-05-08 13:22:18 +0200

    chore: formatting

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { Hono } from 'hono';
-import { cors } from 'hono/cors';
-import { csrf } from 'hono/csrf';
-import { cache } from 'hono/cache';
-import { basicAuth } from 'hono/basic-auth';
-import { prettyJSON } from 'hono/pretty-json';
-import { logger } from 'hono/logger';
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
+import { cache } from "hono/cache";
+import { basicAuth } from "hono/basic-auth";
+import { prettyJSON } from "hono/pretty-json";
+import { logger } from "hono/logger";
 
-import { app as contents } from './controllers/contents';
-import type { Env } from './types';
+import { app as contents } from "./controllers/contents";
+import type { Env } from "./types";
 
 const app = new Hono<{
-  Bindings: Env;
+	Bindings: Env;
 }>();
 
 app.use(logger());
@@ -18,36 +18,36 @@ app.use(logger());
 app.use(prettyJSON());
 
 app.use(
-  cors({
-    origin: ['cleann.dereje.fr'],
-    allowMethods: ['GET', 'POST', 'DELETE'],
-    credentials: true,
-    maxAge: 600,
-    allowHeaders: ['Content Type', 'Authorization'],
-  })
+	cors({
+		origin: ["cleann.dereje.fr"],
+		allowMethods: ["GET", "POST", "DELETE"],
+		credentials: true,
+		maxAge: 600,
+		allowHeaders: ["Content Type", "Authorization"],
+	}),
 );
 
 app.use(
-  csrf({
-    origin: ['cleann.dereje.fr'],
-  })
+	csrf({
+		origin: ["cleann.dereje.fr"],
+	}),
 );
 
-app.use('*', async (c, next) => {
-  const auth = basicAuth({
-    username: c.env.BASIC_USERNAME,
-    password: c.env.BASIC_PASSWORD,
-    verifyUser: async (username, password) => {
-      if (
-        username === c.env.BASIC_USERNAME &&
-        password === c.env.BASIC_PASSWORD
-      ) {
-        return true;
-      }
-      return false;
-    },
-  });
-  return auth(c, next);
+app.use("*", async (c, next) => {
+	const auth = basicAuth({
+		username: c.env.BASIC_USERNAME,
+		password: c.env.BASIC_PASSWORD,
+		verifyUser: async (username, password) => {
+			if (
+				username === c.env.BASIC_USERNAME &&
+				password === c.env.BASIC_PASSWORD
+			) {
+				return true;
+			}
+			return false;
+		},
+	});
+	return auth(c, next);
 });
 
 // app.get(
@@ -58,6 +58,6 @@ app.use('*', async (c, next) => {
 //   })
 // );
 
-app.route('/', contents);
+app.route("/", contents);
 
 export default app;

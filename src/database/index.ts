e0ba22742dcb094f92bc06e92330d69734762157
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-05-08 12:36:09 +0200

    chore: update database index.ts to use single quotes consistently and fix indentation

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,18 +1,18 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import { config } from "dotenv";
-import { categories, contents, contentFlag } from "./schemas/content";
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { config } from 'dotenv';
+import { categories, contents, contentFlag } from './schemas/content';
 
 config({
-	path: "../../.dev.vars",
+  path: '../../.dev.vars',
 });
 
-const sql = neon(process.env.DATABASE_URL || "");
+const sql = neon(process.env.DATABASE_URL || '');
 
 export const db = drizzle(sql, {
-	schema: {
-		contents,
-		categories,
-		content_flag: contentFlag,
-	},
+  schema: {
+    contents,
+    categories,
+    content_flag: contentFlag,
+  },
 });
